43c91ae1a4c13a1cc83e0b5fad2e1a913c79c7e3
Adrian <aadriancz45@gmail.com>
2025-04-15 21:34:02 -0600

    "Remove the Configurator interface and the attachConfigurator method from FragmentConfigurator to give full freedom to the save, update, and delete methods."

--- expandablefragment/src/main/java/com/example/expandablefragment/Configurator.kt
+++
@@ -1,8 +0,0 @@
-package com.example.expandablefragment
-
-interface Configurator {
-    fun save(t: Any)
-    fun update(t: Any)
-    fun delete(t: Any)
-}
-
--- expandablefragment/src/main/java/com/example/expandablefragment/FragmentConfigurator.kt
+++ expandablefragment/src/main/java/com/example/expandablefragment/FragmentConfigurator.kt
@@ -3,13 +3,8 @@ package com.example.expandablefragment
 import androidx.fragment.app.Fragment
 
 abstract class FragmentConfigurator : Fragment() {
-    protected abstract var configurator : Configurator?
-
     abstract fun save()
     abstract fun update()
     abstract fun delete()
 
-    open fun attachConfigurator(cfg : Configurator){
-        configurator = cfg
-    }
 }
